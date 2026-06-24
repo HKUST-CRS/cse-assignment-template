79b573333cc3ce5aad7b682d3856825c3c4d326c
Flandia <me@flandia.dev>
2026-06-24 16:38:18 -0700

    feat(markdown): support rich assignment markdown

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,20 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-
+import remarkGfm from "remark-gfm";
+import remarkAlerts from "remark-alerts";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
 
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  markdown: {
+    remarkPlugins: [remarkGfm, remarkAlerts, remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
